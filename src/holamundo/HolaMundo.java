08a7aeda4a250dcc20c8a7c7e6e80609dc5cad57
José Fílter Cifuentes <jotandj13@gmail.com>
2019-04-01 12:01:29 +0200

    Código fuente inicial

---
+++ src/holamundo/HolaMundo.java
@@ -0,0 +1,10 @@
+package holamundo;
+
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+        System.out.println("Ejemplo para pruebas");
+    }
+
+}
